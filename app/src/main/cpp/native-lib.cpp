f1c9bd89a5241074229e7406dc16b18c547f9e11
lichao3140 <396229938@qq.com>
2018-05-04 09:05:13 +0800

    音频解码播放

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,9 +3,13 @@
 #include <android/log.h>
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, "lichao", __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "lichao", __VA_ARGS__)
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "lichao", __VA_ARGS__)
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, "ChaoPlayer", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "ChaoPlayer", __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ChaoPlayer", __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "ChaoPlayer", __VA_ARGS__)
 
 extern "C"{
     #include <libavcodec/avcodec.h>
@@ -30,6 +34,41 @@ long long GetNowMs() {
     long long t = sec * 1000 + tv.tv_usec / 1000;
     return t;
 }
+/*********************音频播放OpenSLES ****************************/
+//  1.创建引擎
+static SLObjectItf engineSL = NULL;
+SLEngineItf CreateSL() {
+    SLresult re;
+    SLEngineItf en;
+    re = slCreateEngine(&engineSL, 0, 0, 0, 0, 0);// 创建引擎对象
+    if(re != SL_RESULT_SUCCESS) // 失败
+        return NULL;
+    re = (*engineSL)->Realize(engineSL, SL_BOOLEAN_FALSE); //成功   等待对象创建
+    if(re != SL_RESULT_SUCCESS)
+        return NULL;
+    re = (*engineSL)->GetInterface(engineSL, SL_IID_ENGINE, &en);  // 获取接口
+    if(re != SL_RESULT_SUCCESS)
+        return NULL;
+    return en;
+}
+
+void PcmCall(SLAndroidSimpleBufferQueueItf bf, void *context) {
+    LOGI("PcmCall");
+    static FILE *fp = NULL;
+    static char *buf = NULL;
+    if(!buf) {
+        buf = new char[1024*1024];
+    }
+    if(!fp) {
+        fp = fopen("/sdcard/Music/test.pcm", "rb");
+    }
+    if(!fp)return;
+    if(feof(fp) == 0) {
+        int len = fread(buf,1,1024,fp);
+        if(len > 0)
+            (*bf)->Enqueue(bf,buf,len);
+    }
+}
 
 extern "C"
 JNIEXPORT
@@ -43,6 +82,75 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_lichao_chaoplayer_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
     std::string hello = "Hello from C++";
+    //1 创建引擎
+    SLEngineItf eng = CreateSL();
+    if(eng){
+        LOGD("CreateSL success！");
+    }else{
+        LOGD("CreateSL failed！");
+    }
+
+    //2 创建混音器
+    SLObjectItf mix = NULL;
+    SLresult re = 0;
+    re = (*eng)->CreateOutputMix(eng, &mix, 0, 0, 0);  // 1 引擎  2 输出的混音器 345配置项音效
+    if (re != SL_RESULT_SUCCESS) {
+        LOGD("SL_RESULT_SUCCESS failed!");
+    }
+    re = (*mix)->Realize(mix, SL_BOOLEAN_FALSE); // 实例化对象  1 对象  2 等待创建完毕
+    if (re != SL_RESULT_SUCCESS) {
+        LOGD("(*mix)->Realize failed!");
+    }
+    SLDataLocator_OutputMix outMix = {SL_DATALOCATOR_OUTPUTMIX, mix}; // 用来存放声音给音频
+    SLDataSink audioSink= {&outMix, 0};
+
+    //3 配置音频信息
+    //缓冲队列
+    SLDataLocator_AndroidSimpleBufferQueue que = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,10};
+    //音频格式
+    SLDataFormat_PCM pcm = {
+            SL_DATAFORMAT_PCM,
+            2,  // 声道数
+            SL_SAMPLINGRATE_44_1,
+            SL_PCMSAMPLEFORMAT_FIXED_16,
+            SL_PCMSAMPLEFORMAT_FIXED_16,
+            SL_SPEAKER_FRONT_LEFT|SL_SPEAKER_FRONT_RIGHT,
+            SL_BYTEORDER_LITTLEENDIAN // 字节序，小端
+    };
+    SLDataSource ds = {&que,&pcm};
+
+    //4 创建播放器
+    SLObjectItf player = NULL;
+    SLPlayItf iplayer = NULL;
+    SLAndroidSimpleBufferQueueItf pcmQue = NULL;  // 存放地址
+    const SLInterfaceID ids[] = {SL_IID_BUFFERQUEUE};
+    const SLboolean req[] = {SL_BOOLEAN_TRUE};
+    // 创建播放器 1 引擎  2 播放器对象  3 数据源信息  4 混音器  5   6  7
+    re = (*eng)->CreateAudioPlayer(eng, &player, &ds, &audioSink, sizeof(ids) / sizeof(SLInterfaceID), ids, req);
+    if(re != SL_RESULT_SUCCESS) {
+        LOGD("CreateAudioPlayer failed!");
+    } else {
+        LOGD("CreateAudioPlayer success!");
+    }
+    (*player)->Realize(player, SL_BOOLEAN_FALSE); // 实例化
+    //获取player接口
+    re = (*player)->GetInterface(player, SL_IID_PLAY,&iplayer);
+    if(re != SL_RESULT_SUCCESS) {
+        LOGD("GetInterface SL_IID_PLAY failed!");
+    }
+    re = (*player)->GetInterface(player, SL_IID_BUFFERQUEUE, &pcmQue); // 获取播放用的接口
+    if(re != SL_RESULT_SUCCESS) {
+        LOGD("GetInterface SL_IID_BUFFERQUEUE failed!");
+    }
+
+    //设置回调函数，播放队列空调用
+    (*pcmQue)->RegisterCallback(pcmQue, PcmCall, 0);
+
+    //设置为播放状态
+    (*iplayer)->SetPlayState(iplayer, SL_PLAYSTATE_PLAYING);
+
+    //启动队列回调
+    (*pcmQue)->Enqueue(pcmQue, "", 1);
 
     return env->NewStringUTF(hello.c_str());
 }
