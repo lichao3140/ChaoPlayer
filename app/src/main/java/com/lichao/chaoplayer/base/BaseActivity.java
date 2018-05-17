4e4fdd677f767017435c8f8fbf30f7afec52d7ee
lichao3140 <396229938@qq.com>
2018-05-17 21:49:55 +0800

    添加熊猫直播

---
+++ app/src/main/java/com/lichao/chaoplayer/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.lichao.chaoplayer.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ChaoLi on 2018/5/17 0017 - 21:48
+ * Email: lichao3140@gmail.com
+ * Version: v1.0
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/lichao/chaoplayer/base/BaseFragment.java
@@ -0,0 +1,12 @@
+package com.lichao.chaoplayer.base;
+
+import android.app.Fragment;
+
+/**
+ * Created by ChaoLi on 2018/5/17 0017 - 21:45
+ * Email: lichao3140@gmail.com
+ * Version: v1.0
+ */
+public abstract class BaseFragment extends Fragment {
+
+}
